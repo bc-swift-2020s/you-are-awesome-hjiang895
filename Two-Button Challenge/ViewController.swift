ab5258a73a9f79f45f0c817a97f805c01eacac8f
Hannah Jiang <jiangbz@bc.edu>
2020-01-25 21:33:53 -0500

    Section 2.4.1 - Two-Button Challenge (added in text alignment)

--- Two-Button Challenge/ViewController.swift
+++ Two-Button Challenge/ViewController.swift
@@ -19,10 +19,14 @@ class ViewController: UIViewController {
 
     @IBAction func leftButtonPressed(_ sender: UIButton) {
         messageLabel.text = "You Are Awesome!"
+        messageLabel.textColor = UIColor.blue
+        messageLabel.textAlignment = NSTextAlignment.left
     }
     
     @IBAction func rightButtonPressed(_ sender: UIButton) {
         messageLabel.text = "You Are Great!"
+        messageLabel.textColor = UIColor.systemRed
+        messageLabel.textAlignment = NSTextAlignment.right
     }
     
 }
