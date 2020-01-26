81ae7b61f695f1bd0b8efcb3e4e716289283cc6a
Hannah Jiang <jiangbz@bc.edu>
2020-01-26 13:14:12 -0500

    Section 2.10 - Randomness and Scaling

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     
     var imageNumber = 0
     var messageNumber = 0
+    let totalNumberOfImages = 9
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,23 +23,29 @@ class ViewController: UIViewController {
         
     }
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        let messages = ["You Are Awesome!", "You Are Great!", "You Are Fantastic?", "Fantastic? That's you!"]
-        messageLabel.text = messages[messageNumber]
-        messageNumber += 1
-        if messageNumber == messages.count {
-            messageNumber = 0
-        }
+        let messages = ["You Are Awesome!",
+                        "You Are Great!",
+                        "You Are Fantastic?",
+                        "Fantastic? That's you!",
+                        "When the genius bar needs help, they call you",
+                        "You've got the design skills of Jony Ive"]
+        messageLabel.text = messages[Int.random(in: 0...messages.count - 1)]
+        imageView.image = UIImage(named: "image\(Int.random(in: 0...totalNumberOfImages))")
+//        messageNumber += 1
+//        if messageNumber == messages.count {
+//            messageNumber = 0
+//        }
+//
         
         
+//        print(imageNumber)
+//        //let imageName = "image" + String(imageNumber)
+//        let imageName = "image\(imageNumber)"
+//        imageView.image = UIImage(named: imageName)
+//        imageNumber = imageNumber + 1
+//        if imageNumber == 10 {
+//            imageNumber = 0
         
-        print(imageNumber)
-        //let imageName = "image" + String(imageNumber)
-        let imageName = "image\(imageNumber)"
-        imageView.image = UIImage(named: imageName)
-        imageNumber = imageNumber + 1
-        if imageNumber == 10 {
-            imageNumber = 0
-        }
         
         //        let awesomeMessage = "You Are Awesome!"
         //        let greatMessage = "You are Great!"
